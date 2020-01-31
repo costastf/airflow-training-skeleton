1e9f88bd215dafbd979bae3554cd30b40fe34fe4
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-01-31 16:20:27 +0100

    sdfs

--- dags/exercise5.py
+++ dags/exercise5.py
@@ -19,14 +19,15 @@
 
 """Example DAG demonstrating the usage of the BashOperator."""
 
-import airflow
 import datetime
+from datetime import timedelta
+
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
 from airflow.models import DAG
 
 arguments = {'dag_id': 'exercise5',
              'default_args': {'owner': 'Costas',
-                              'start_date': datetime.datetime.today()},
+                              'start_date': datetime.datetime.today() - timedelta(days=1)},
              'schedule_interval': None}
 
 with DAG(**arguments) as dag:
