e769b9c3c163a504ddc2557170faa2cd7c0a137c
Costas Tyfoxylos <costas.tyf@gmail.com>
2020-02-14 11:32:54 +0100

    sgafs

--- dags/exercise6.py
+++ dags/exercise6.py
@@ -78,7 +78,7 @@ class LaunchLibraryOperator(BaseOperator):
         temp = NamedTemporaryFile(mode='w+t')
         temp.write(json.dumps(file))
         hook.upload(self.bucket, self.result_key, temp.name,  'application/json', False)
-        # temp.close()
+        temp.close()
 
 
 arguments = {'dag_id': 'exercise6',
